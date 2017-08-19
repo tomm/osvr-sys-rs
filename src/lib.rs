3eb0b991179e3ff0eeb8183a3671627c74a6f97d
Tom Morton <tomm@riseup.net>
2017-08-19 17:44:05 +0100

    sdl2 dependency so SDL_GL_GetProcAddress links

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #[allow(dead_code)]
+extern crate sdl2;
 
 extern "C" {
     pub fn SDL_GL_GetProcAddress(name: *const ::std::os::raw::c_char) -> *const ::std::os::raw::c_void;
